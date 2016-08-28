b518bc3af5f5f1f4f6bdb49153c38b3fa0d3da7d
Billard <sirfrancisbillard@gmail.com>
2016-08-28 01:44:08 -0700

    Create industrial_laser.lua

---
+++ industrial/lua/weapons/industrial_laser.lua
@@ -0,0 +1,14 @@
+AddCSLuaFile()
+
+SWEP.PrintName = "Laser Rifle"
+SWEP.Category = "Industrial Weapons"
+SWEP.Instructions = "Deals heavy ranged damage.\nCan be charged at a charging station."
+
+SWEP.Spawnable = true
+SWEP.AdminOnly = false
+
+SWEP.Slot = 3
+
+SWEP.WorldModel = "models/weapons/w_irifle.mdl"
+SWEP.ViewModel = "models/weapons/c_irifle.mdl"
+SWEP.UseHands = true
