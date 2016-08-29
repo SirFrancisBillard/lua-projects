8a9c4e70e46fa0703d8560087a96030b4b162fc8
Billard <sirfrancisbillard@gmail.com>
2016-08-29 10:27:06 -1000

    Update industrial_crafting_table.lua

--- industrial/lua/entities/industrial_crafting_table.lua
+++ industrial/lua/entities/industrial_crafting_table.lua
@@ -2,7 +2,7 @@ AddCSLuaFile()
 
 ENT.Base = "industrial_base"
 ENT.PrintName = "Crafting Table"
-ENT.Spawnable = true6
+ENT.Spawnable = true
 ENT.Model = "models/props/CS_militia/wood_table.mdl"
 function ENT:IndustrialType()
 	return "mach"
@@ -16,19 +16,8 @@ function ENT:Recipes()
 	return 0
 end
 function ENT:HelpText()
-	return "Crafting tables are used to forge items like jetpacks and laser guns using materials and power."
+	return "Crafting tables are used to forge items such as jetpacks, lasers, and higher tier materials."
 end
 function ENT:GetMaxStoredPower()
 	return 10000
-end
-function ENT:ExtraNetworkedVars()
-	self:NetworkVar("Entity", 0, "Crafter")
-end
-if SERVER then
-	function ENT:ExtraThink() end
-	function ENT:OnEntityUsed(ply)
-		if (ply:IsPlayer()) then
-			self:SetCrafter(ply)
-		end
-	end
 end
