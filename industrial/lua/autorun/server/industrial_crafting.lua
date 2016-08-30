6f8aa4444cf752f9954b01b25c270bdb12993591
Billard <sirfrancisbillard@gmail.com>
2016-08-29 21:56:52 -1000

    Update industrial_crafting.lua

--- industrial/lua/autorun/server/industrial_crafting.lua
+++ industrial/lua/autorun/server/industrial_crafting.lua
@@ -5,9 +5,11 @@ local recipes = {
 	["jetpack"] = {result = "industrial_jetpack", recipe = {"industrial_metal", "industrial_metal", "industrial_fuel"}, power = 6000, ctable = "industrial_crafting_table", func = function(ent) end},
 	["laser"] = {result = "industrial_laser", recipe = {"industrial_gold", "industrial_fuel"}, power = 6000, ctable = "industrial_crafting_table", func = function(ent) end},
 	["medicine"] = {result = "industrial_medicide", recipe = {"industrial_zinc", "industrial_zinc"}, power = 400, ctable = "industrial_crafting_table", func = function(ent) end},
-	["advanced alloy"] = {result = "industrial_adv_alloy", recipe = {"industrial_metal", "industrial_metal", "industrial_metal", "industrial_gold"}, power = 800, ctable = "industrial_crafting_table", func = function(ent) end},
+	["advanced alloy"] = {result = "industrial_adv_alloy", recipe = {"industrial_metal", "industrial_metal", "industrial_metal", "industrial_gold"}, power = 1200, ctable = "industrial_crafting_table", func = function(ent) end},
 	["brass"] = {result = "industrial_brass", recipe = {"industrial_metal", "industrial_zinc"}, power = 400, ctable = "industrial_crafting_table", func = function(ent) end}
-
+	["empty missile casing"] = {result = "industrial_missile_empty", recipe = {"industrial_adv_alloy", "industrial_fuel"}, power = 4000, ctable = "industrial_munition_table", func = function(ent) end}
+	["napalm missile"] = {result = "industrial_missile_napalm", recipe = {"industrial_missile_empty", "industrial_fuel", "industrial_fuel"}, power = 10000, ctable = "industrial_munition_table", func = function(ent) end}
+	["nuclear fuel rod"] = {result = "industrial_fuel_rod", recipe = {"industrial_adv_alloy", "industrial_uranium"}, power = 20000, ctable = "industrial_munition_table", func = function(ent) end}
 }
 
 function plyMeta:NearestItem(item)
