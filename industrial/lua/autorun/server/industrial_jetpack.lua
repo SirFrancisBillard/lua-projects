b5f82e8dc774cd7ac07094f625f975b2662e5eab
Billard <sirfrancisbillard@gmail.com>
2016-09-03 11:31:02 -1000

    Update industrial_jetpack.lua

--- industrial/lua/autorun/server/industrial_jetpack.lua
+++ industrial/lua/autorun/server/industrial_jetpack.lua
@@ -1,9 +1,5 @@
 hook.Add("StartCommand", "IndustrialMod_Jetpack", function(ply, cmd)
 	if ply:GetNWBool("IsWearingJetpack") and (cmd:KeyDown(IN_JUMP)) then
-		ply:SetVelocity(Vector(0, 0, 25))
+		ply:SetVelocity(Vector(0, 0, 25 + (5 * ply:GetNWInt("Upgrade_JetpackPower"))))
 	end
-end)
-
-hook.Add("DoPlayerDeath", "IndustrialMod_StripJetpack", function(ply, atk, dmg)
-	ply:SetNWBool("IsWearingJetpack", false)
 end)
