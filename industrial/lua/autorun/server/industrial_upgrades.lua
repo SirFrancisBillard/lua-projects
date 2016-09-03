e2f77588cf6fcc6d0e58c63ba5edee1ebd773669
Billard <sirfrancisbillard@gmail.com>
2016-09-03 11:46:31 -1000

    Update industrial_upgrades.lua

--- industrial/lua/autorun/server/industrial_upgrades.lua
+++ industrial/lua/autorun/server/industrial_upgrades.lua
@@ -8,11 +8,13 @@ hook.Add("DoPlayerDeath", "IndustrialMod_ResetNetwork", function(ply, atk, dmg)
 		ply:SetNWBool("IsWearingNanoSuit", false)
 		
 		ply:SetNWInt("Upgrade_NanoSuitProtection", 0)
+		ply:SetNWInt("Upgrade_NanoSuitRespiration", 0)
 	end
 	if (not ply:GetNWBool("Upgrade_QuantumSuitKeep")) then
 		ply:SetNWBool("IsWearingQuantumSuit", false)
 		
 		ply:SetNWInt("Upgrade_QuantumSuitProtection", 0)
+		ply:SetNWInt("Upgrade_QuantumSuitRespiration", 0)
 	end
 	if (not ply:GetNWBool("Upgrade_LaserKeep")) then
 		ply:SetNWInt("Upgrade_LaserDamage", 0)
@@ -22,5 +24,22 @@ hook.Add("DoPlayerDeath", "IndustrialMod_ResetNetwork", function(ply, atk, dmg)
 end)
 
 hook.Add("EntityTakeDamage", "IndustrialMod_SuitProtection", function(target, dmg)
-
+	local atk = dmg:GetAttacker()
+	if target:IsPlayer() then
+		if (dmg:GetDamageType() == DMG_DROWN) then
+			if target:GetNWBool("IsWearingNanoSuit", false) and (target:GetNWInt("Upgrade_NanoSuitRespiration") > 0) then
+				dmg:ScaleDamage(math.max(1.0 / target:GetNWInt("Upgrade_NanoSuitRespiration", 0), 0.01))
+			end
+			if target:GetNWBool("IsWearingQuantumSuit", false) and (target:GetNWInt("Upgrade_QuantumSuitRespiration") > 0) then
+				dmg:ScaleDamage(math.max(1.0 / target:GetNWInt("Upgrade_QuantumSuitRespiration", 0), 0.01))
+			end
+		else
+			if target:GetNWBool("IsWearingNanoSuit", false) and (target:GetNWInt("Upgrade_NanoSuitRespiration") > 0) then
+				dmg:ScaleDamage(math.max(0.4 / target:GetNWInt("Upgrade_NanoSuitProtection", 0), 0.05))
+			end
+			if target:GetNWBool("IsWearingQuantumSuit", false) and (target:GetNWInt("Upgrade_QuantumSuitRespiration") > 0) then
+				dmg:ScaleDamage(math.max(0.1 / target:GetNWInt("Upgrade_QuantumSuitProtection", 0), 0.05))
+			end
+		end
+	end
 end)
