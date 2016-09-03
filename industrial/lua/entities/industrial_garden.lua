9223392b6bbc9caa89b54a277d7ea776123c7236
Billard <sirfrancisbillard@gmail.com>
2016-09-03 13:23:26 -1000

    Create industrial_garden.lua

---
+++ industrial/lua/entities/industrial_garden.lua
@@ -0,0 +1,20 @@
+AddCSLuaFile()
+
+ENT.Base = "industrial_base"
+ENT.PrintName = "Artificial Garden"
+ENT.Spawnable = true
+ENT.Model = "models/props_wasteland/gaspump001a.mdl"
+function ENT:IndustrialType()
+	return "mach"
+	// valid types and their uses
+	// base - does nothing
+	// gen - generates power
+	// bat - stores power
+	// mach - uses power
+end
+function ENT:HelpText()
+	return "Artificial gardens will grow plants when given power."
+end
+function ENT:MinerData()
+	return true, {"industrial_foliage", "industrial_foliage", "industrial_foliage", "industrial_wood", "industrial_wood", "industrial_wood", "industrial_wood", "industrial_flower"}, true, 20, 5
+end
