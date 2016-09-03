574b49bbb847fde27843b9981d92dddfc90822ac
Billard <sirfrancisbillard@gmail.com>
2016-09-03 12:28:49 -1000

    Update industrial_crafting.lua

--- industrial/lua/autorun/server/industrial_crafting.lua
+++ industrial/lua/autorun/server/industrial_crafting.lua
@@ -6,6 +6,7 @@ local recipes = {
 	["laser"] = {result = "industrial_laser", recipe = {"industrial_gold", "industrial_fuel"}, power = 6000, ctable = "industrial_crafting_table", func = function(ent) end},
 	["medicine"] = {result = "industrial_medicide", recipe = {"industrial_zinc", "industrial_honey"}, power = 400, ctable = "industrial_crafting_table", func = function(ent) end},
 	["carbon mesh"] = {result = "industrial_carbon_mesh", recipe = {"industrial_coal_dust", "industrial_coal_dust"}, power = 600, ctable = "industrial_crafting_table", func = function(ent) end},
+	["golden battery"] = {result = "industrial_battery_gold", recipe = {"industrial_battery", "industrial_gold", "industrial_gold"}, power = 1200, ctable = "industrial_crafting_table", func = function(ent) end},
 	["advanced alloy"] = {result = "industrial_adv_alloy", recipe = {"industrial_metal", "industrial_metal", "industrial_metal", "industrial_gold"}, power = 1200, ctable = "industrial_crafting_table", func = function(ent) end},
 	["brass"] = {result = "industrial_brass", recipe = {"industrial_metal", "industrial_zinc"}, power = 400, ctable = "industrial_crafting_table", func = function(ent) end}
 	["empty missile casing"] = {result = "industrial_missile_empty", recipe = {"industrial_adv_alloy", "industrial_fuel"}, power = 4000, ctable = "industrial_munition_table", func = function(ent) end}
