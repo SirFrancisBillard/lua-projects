57918565b70833ac767bc924d862d03947557302
Billard <sirfrancisbillard@gmail.com>
2016-09-03 11:02:03 -1000

    Create industrial_engine_coal.lua

---
+++ industrial/lua/entities/industrial_engine_coal.lua
@@ -0,0 +1,23 @@
+AddCSLuaFile()
+
+ENT.Base = "industrial_base"
+ENT.PrintName = "Coal Engine"
+ENT.Spawnable = true
+ENT.Model = "models/props_vehicles/generatortrailer01.mdl"
+function ENT:IndustrialType()
+	return "gen"
+	// valid types and their uses
+	// base - does nothing
+	// gen - generates power
+	// bat - stores power
+	// mach - uses power
+end
+function ENT:HelpText()
+	return "Coal engines generate power at a normal rate, but require coal to function."
+end
+function ENT:EngineData()
+	return true, {"industrial_coal"}, {40}
+end
+function ENT:PowerGenerationRate()
+	return 20
+end
