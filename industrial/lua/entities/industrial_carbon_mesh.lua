118fd4652d037d8c02b0b2427e19ee0358d86a45
Billard <sirfrancisbillard@gmail.com>
2016-09-03 11:09:39 -1000

    Create industrial_carbon_mesh.lua

---
+++ industrial/lua/entities/industrial_carbon_mesh.lua
@@ -0,0 +1,26 @@
+AddCSLuaFile()
+
+ENT.Base = "industrial_base"
+ENT.PrintName = "Carbon Mesh"
+ENT.Spawnable = true
+ENT.Model = "models/props_junk/rock001a.mdl"
+function ENT:IndustrialType()
+	return "base"
+	// valid types and their uses
+	// base - does nothing
+	// gen - generates power
+	// bat - stores power
+	// mach - uses power
+end
+function ENT:HelpText()
+	return "Carbon mesh can be compressed to create carbon plates.
+end
+function ENT:CanReceivePower()
+	return false
+end
+function ENT:CanTransmitPower()
+	return false
+end
+function ENT:PermaMaterial()
+	return "models/props/cs_office/snowmana"
+end
