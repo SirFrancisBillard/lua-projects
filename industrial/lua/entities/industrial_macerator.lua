559610559eab667eeb94c53a6ff95a888df0e7aa
Billard <sirfrancisbillard@gmail.com>
2016-09-03 10:59:02 -1000

    Create industrial_macerator.lua

---
+++ industrial/lua/entities/industrial_macerator.lua
@@ -0,0 +1,20 @@
+AddCSLuaFile()
+
+ENT.Base = "industrial_base"
+ENT.PrintName = "Macerator"
+ENT.Spawnable = true
+ENT.Model = "models/props/CS_militia/furnace01.mdl"
+function ENT:IndustrialType()
+	return "mach"
+	// valid types and their uses
+	// base - does nothing
+	// gen - generates power
+	// bat - stores power
+	// mach - uses power
+end
+function ENT:HelpText()
+	return "Some items can be ground down into more usable materials."
+end
+function ENT:RefineryData()
+	return true, {"industrial_coal", "industrial_metal", "industrial_foliage"}, 1, {"industrial_coal_dust", "industrial_sand", "industrial_biofuel"}, 10, 20, false
+end
