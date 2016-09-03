7cc0e773851133befd460ccb2d0dea90cbe90fe1
Billard <sirfrancisbillard@gmail.com>
2016-09-03 13:25:52 -1000

    Create industrial_wood.lua

---
+++ industrial/lua/entities/industrial_wood.lua
@@ -0,0 +1,26 @@
+AddCSLuaFile()
+
+ENT.Base = "industrial_base"
+ENT.PrintName = "Wood"
+ENT.Spawnable = true
+ENT.Model = "models/Items/item_item_crate_chunk02.mdl"
+function ENT:IndustrialType()
+	return "base"
+	// valid types and their uses
+	// base - does nothing
+	// gen - generates power
+	// bat - stores power
+	// mach - uses power
+end
+function ENT:HelpText()
+	return "Wood can be used to make forestry machines."
+end
+function ENT:CanReceivePower()
+	return false
+end
+function ENT:CanTransmitPower()
+	return false
+end
+function ENT:PermaMaterial()
+	return "models/props_pipes/GutterMetal01a"
+end
