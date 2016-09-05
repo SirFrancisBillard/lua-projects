1f146e13664b3c595caa1a2af85a10fe31d2cd5a
Billard <sirfrancisbillard@gmail.com>
2016-09-04 15:57:17 -1000

    HUD update to show progress

--- industrial/lua/autorun/client/industrial_hint.lua
+++ industrial/lua/autorun/client/industrial_hint.lua
@@ -10,6 +10,19 @@ hook.Add("HUDPaint", "IndustrialMod_DrawHints", function()
 	if IsValid(trent) and (trent.GetStoredPower != nil) and (trent:IndustrialType() != "base") and (trent:GetPos():Distance(LocalPlayer():EyePos()) < 256) then
 		draw.SimpleTextOutlined("Power: "..trent:GetStoredPower().."/"..trent:GetMaxStoredPower(), "Default", halfScrW, halfScrH + 25, clrWhite, 1, 1, 1, clrBlack)
 	end
+	if IsValid(trent) and ((trent.GetConvertCooldown != nil) or (trent.GetMiningCooldown != nil)) and (trent:IndustrialType() != "base") and (trent:GetPos():Distance(LocalPlayer():EyePos()) < 256) then
+		local a, b
+		if (trent.GetConvertCooldown != nil) then
+			local IsRef, Mats, MatAmt, Prod, Tim, Pow, MultiProd = trent:RefineryData()
+			a = Tim - trent:GetConvertCooldown()
+			b = Tim
+		elseif (trent.GetMiningCooldown != nil) then
+			local IsMiner, Stuff, IsRandom, mTim, mPow = trent:MinerData()
+			a = mTim - trent:GetMiningCooldown()
+			b = mTim
+		end
+		draw.SimpleTextOutlined("Progress: "..a.."/"..b, "Default", halfScrW, halfScrH + 50, clrWhite, 1, 1, 1, clrBlack)
+	end
 	if IsValid(trent) and (trent.IndustrialType != nil) and (trent:GetPos():Distance(LocalPlayer():EyePos()) < 256) then
 		draw.SimpleTextOutlined(scripted_ents.Get(trent:GetClass()).PrintName, "Default", halfScrW, halfScrH - 25, clrWhite, 1, 1, 1, clrBlack)
 	end
