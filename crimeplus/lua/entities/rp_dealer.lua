b990be4c079769e4df23b95f11267037e3d5f150
Zig <zigbomb@outlook.com>
2016-09-11 21:27:21 -0400

    Update rp_dealer.lua
    updated that shit

--- crimeplus/lua/entities/rp_dealer.lua
+++ crimeplus/lua/entities/rp_dealer.lua
@@ -5,12 +5,13 @@ ENT.Base = "base_gmodentity"
 ENT.PrintName = "Drug Addict"
 ENT.Category = "Crime+"
 ENT.Spawnable = true
-ENT.Model = "models/Humans/Group01/male_08.mdl"
+ENT.Model = "models/player/group03/male_08.mdl"
 
 function ENT:Initialize()
 	self:SetModel(self.Model)
 	self:SetMoveType(MOVETYPE_VPHYSICS)
 	self:SetSolid(SOLID_VPHYSICS)
+
 	if SERVER then
 		self:PhysicsInit(SOLID_VPHYSICS)
 		self:SetUseType(SIMPLE_USE)
@@ -24,19 +25,16 @@ end
 if SERVER then
 	function ENT:Use(activator, caller)
 		if IsValid(caller) and caller:IsPlayer() then
-			local randy = math.random(1, 5)
+			local randy = math.random(1, 2)
 			if (randy == 1) then
-				caller:ChatPrint("HELP GET THESE BEES OUT OF MY TEETH!")
+				caller:ChatPrint("What the fuck do you want?")
 			elseif (randy == 2) then
-				caller:ChatPrint("I AM IN THE WRONG LINE AT K-MART")
-			elseif (randy == 3) then
-				caller:ChatPrint("WHAT ARE YOU DOING WITH THAT RHINOCEROUS")
-			elseif (randy == 4) then
-				caller:ChatPrint("NOT THE PURPLE")
-			elseif (randy == 5) then
-				caller:ChatPrint("WHY DO BANANAS HAVE TENTACLES")
+				caller:ChatPrint("Get lost, buddy.")
 			end
-			self:EmitSound("vo/npc/male01/help01.wav")
 		end
 	end
+end
+
+function ENT:Think()
+	self:SetSequence("idle_all_02")
 end
