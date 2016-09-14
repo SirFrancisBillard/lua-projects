34da0defe27d6f84277026874ce8cf13ff00a733
Billard <sirfrancisbillard@gmail.com>
2016-09-13 20:07:12 -1000

    Prettier car bomb

--- awesome_sweps/lua/weapons/billard_car_bomb.lua
+++ awesome_sweps/lua/weapons/billard_car_bomb.lua
@@ -25,8 +25,10 @@ SWEP.Secondary.Automatic = false
 function SWEP:SetupDataTables()
 	self:NetworkVar("Int", 0, "DetonateTime")
 	self:NetworkVar("Int", 1, "ReloadSpamTime")
+	self:NetworkVar("Int", 2, "PlantingProgress")
 	self:NetworkVar("Bool", 0, "BombType")
 	self:NetworkVar("Bool", 1, "Deploying")
+	self:NetworkVar("Bool", 2, "Planting")
 end
 
 function SWEP:CanSecondaryAttack()
@@ -37,7 +39,7 @@ function SWEP:CanPrimaryAttack()
 	self.Owner:LagCompensation(true)
 	local trent = self.Owner:GetEyeTrace().Entity
 	self.Owner:LagCompensation(false)
-	return (not self:GetDeploying()) (not trent.HasCarBombPlanted) and IsValid(trent) and trent:IsVehicle() and (self.Owner:GetPos():Distance(trent:GetPos()) < 512)
+	return (not self:GetDeploying()) and (not trent.HasCarBombPlanted) and IsValid(trent) and trent:IsVehicle() and (self.Owner:GetPos():Distance(trent:GetPos()) < 512)
 end
 
 function SWEP:Deploy()
@@ -52,9 +54,15 @@ function SWEP:Deploy()
 	return true
 end
 
+function SWEP:Holster()
+	self:SetPlanting(false)
+	self:SetPlantingProgress(0)
+	return true
+end
+
 function SWEP:Reload()
-	if (self:GetReloadSpamTime() <= CurTime()) then
-		self:SetReloadSpamTime(CurTime() + 0.5)
+	if (self:GetReloadSpamTime() < CurTime()) then
+		self:SetReloadSpamTime(CurTime() + 1)
 		self:EmitSound(Sound("weapons/c4/c4_beep1.wav"))
 		if self:GetDetonateTime() < 26 then
 			self:SetDetonateTime(self:GetDetonateTime() + 5)
@@ -78,11 +86,25 @@ function SWEP:SecondaryAttack()
 end
 
 function SWEP:PrimaryAttack()
-	if (not self:CanPrimaryAttack()) then return end
+	if (not self:CanPrimaryAttack()) or self:GetPlanting() then return end
+	self:SetPlanting(true)
 	self:SetNextPrimaryFire(CurTime() + 1)
 	self.Weapon:SendWeaponAnim(ACT_VM_PRIMARYATTACK)
+	for i = ((self:SequenceDuration() + 0.1) / 100), (self:SequenceDuration() + 0.1), ((self:SequenceDuration() + 0.1) / 100) do
+		timer.Simple(i, function()
+			if IsValid(self) and IsValid(self.Owner) then
+				self:SetPlantingProgress(self:GetPlantingProgress() + 1)
+			end
+		end)
+	end
 	timer.Simple(self:SequenceDuration() + 0.1, function()
-		if (not self:CanPrimaryAttack()) or (not IsValid(self)) then return end
+		if IsValid(self) and (not self:CanPrimaryAttack()) then
+			self.Weapon:SendWeaponAnim(ACT_VM_IDLE)
+			self:SetPlanting(false)
+			self:SetPlantingProgress(0)
+			return
+		end
+		if (not IsValid(self)) then return end
 		if IsValid(self.Owner) and IsValid(self.Owner:GetActiveWeapon()) and (self.Owner:GetActiveWeapon():GetClass() == self.ClassName) then
 			self:EmitSound(Sound("weapons/c4/c4_plant.wav"))
 			if self.Owner:IsPlayer() then
@@ -115,4 +137,22 @@ function SWEP:PrimaryAttack()
 			end
 		end
 	end)
+end
+
+if CLIENT then
+	function SWEP:DrawHUD()
+		if self:GetPlanting() then
+			local color_r = math.ceil(((self:GetPlantingProgress() / 100) * -255) + 255)
+			local color_g = math.ceil((self:GetPlantingProgress() / 100) * 255)
+			print(color_r.." - "..color_g)
+			local width = 200
+			local height = 60
+			local border = 5
+			local master_h = (ScrH() / 2) - 200
+			local master_w = (ScrW() / 2) - (width / 2)
+			draw.RoundedBox(8, master_w - border, master_h - border, width + (border * 2), height + (border * 2), Color(0, 0, 0))
+			draw.RoundedBox(8, master_w, master_h, (width / 100) * self:GetPlantingProgress(), height, Color(0, 0, 255))
+			draw.SimpleText("Planting...", "Trebuchet24", master_w + 55, master_h + 20, Color(255, 255, 255))
+		end
+	end
 end
