c2c6c65d10c5e6a954d6572655373c74c93ce06a
Billard <sirfrancisbillard@gmail.com>
2016-09-14 21:26:06 -1000

    3D2D Stove

--- crimeplus/lua/entities/rp_stove.lua
+++ crimeplus/lua/entities/rp_stove.lua
@@ -29,6 +29,7 @@ end
 if SERVER then
 	function ENT:StartTouch(ent)
 		if IsValid(ent) and (ent:GetClass() == "rp_gas") and (not self:GetHasCanister()) and (not ent:GetHasStove()) then
+			if (not ent:IsReadyForStove()) then return end
 			self:SetHasCanister(true)
 			self:SetCanister(ent)
 			ent:SetHasStove(true)
@@ -36,5 +37,24 @@ if SERVER then
 			local weld = constraint.Weld(self, ent, 0, 0, 0, true, false)
 			self:EmitSound("physics/metal/metal_barrel_impact_soft"..math.random(1, 4)..".wav")
 		end
+	end
+end
+
+if CLIENT then
+	function ENT:Draw()
+		self:DrawModel()
+
+		local Pos = self:GetPos()
+		local Ang = self:GetAngles()
+
+		surface.SetFont("Trebuchet24")
+
+		Ang:RotateAroundAxis(Ang:Forward(), 90)
+		Ang:RotateAroundAxis(Ang:Right(), -90)
+
+		cam.Start3D2D(Pos + (Ang:Up() * 18) + (Ang:Right() * -2), Ang, 0.3)
+			draw.WordBox(2, -30, -50, "Stove", "Trebuchet24", Color(140, 0, 0, 100), Color(255, 255, 255, 255))
+			draw.WordBox(2, -42.5, -10, "Gas: "..(self:GetHasCanister() and self:GetCanister():GetFuel() or "None"), "Trebuchet24", Color(140, 0, 0, 100), Color(255, 255, 255, 255))
+		cam.End3D2D()
 	end
 end
