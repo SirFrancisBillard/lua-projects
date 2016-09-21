675b4b71d95ee8d323b7bc364f66ce9d1d4804c2
Billard <sirfrancisbillard@gmail.com>
2016-09-21 11:57:05 -1000

    Small config

---
+++ advanced_meth/lua/autorun/aml_config.lua
@@ -0,0 +1,17 @@
+-------------------------------------------
+-- ADVANCED METH LAB CONFIGURATION FILE ---
+-------------------------------------------
+
+-- Time config
+-- The time it takes for things to cook in the pot
+AML_CONFIG_TIME_POT = 30
+-- The time it takes for things to cook in the small pot
+AML_CONFIG_TIME_SMALL_POT = 15
+-- The time it takes for things to mix in the barrel
+AML_CONFIG_TIME_BARREL = 20
+
+-- Smoke config
+-- The regular color of smoke
+AML_CONFIG_SMOKE_REGULAR = Color(125, 125, 125)
+-- The color of smoke when cooking Red Acid
+AML_CONFIG_SMOKE_RED = Color(200, 0, 0)
