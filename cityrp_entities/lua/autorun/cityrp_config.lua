e84524d58ef4eda000d84f2009c6901683623bda
Billard <sirfrancisbillard@gmail.com>
2016-10-06 10:59:51 -1000

    CityRP Config

---
+++ cityrp_entities/lua/autorun/cityrp_config.lua
@@ -0,0 +1,29 @@
+-- CityRP Entities configuration file
+
+CityRP = {
+	Config = {
+		C4 = {
+			Sounds = {
+				Beep = Sound("weapons/c4/c4_beep1.wav"),
+				Plant = Sound("weapons/c4/c4_plant.wav"),
+				Defuse = Sound("weapons/c4/c4_disarm.wav"),
+				Explode = Sound("phx/explode00.wav")
+			},
+			Values = {
+				Timer = 30
+			}
+		},
+		Defuser = {
+			Sounds = {
+				Equip = Sound("items/defuser_equip.wav")
+			},
+			Values = {
+				BreakChance = 40
+			}
+		},
+		Molotov = {
+			Sounds = {},
+			Values = {}
+		}
+	}
+}
