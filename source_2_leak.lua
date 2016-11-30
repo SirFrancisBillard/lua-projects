f57185037c22cd78a597fa7d9f91391eeb30360f
Sir Francis Billard <sirfrancisbillard@gmail.com>
2016-11-29 19:31:20 -1000

    SOURCE TWO LEAKED SECRET GABEN SOURCE TWO NEWS TWENTY EIGHTEEN

---
+++ source_2_leak.lua
@@ -0,0 +1,37 @@
+words = "Weather lookin' good?"
+words = words:lower()
+transy = words:gsub("?", ", eh?")
+print(transy)
+
+-- ADVANCED SOURCE 2 GRAPHICS ENGINE LEAK 2016 NEWS SOURCE 2 GABEN CONFIRMED
+
+print([[
+mmmddddddddddddddddddddddhhhhhdddhhhhddhhhdddddddddddddddddd
+mmdddddddddddddddddddhhhddmmmmmdmmmmdmdddhhhhddddddddddddddd
+mmddddddddddddddddhhhdmNmmddddmmmNNNNNNmmdhhhhhddddddddddddd
+dddddddddddddddddhhdmmmddddhhddmmmNNNmmNNNmddhhhhhdddddddddd
+dddddddddddddddhhhmNNmNNmmddddddhdmmmmmmNNNmhdhhhhhhdddddddd
+ddddddddddddddhhhmNNNNmmmmmmhhhhddddmmmmmmmmdddhhhhhhddddddd
+dddddddddddddhhhmNNNNmmmddhyoo+ossosyssyhyyhyyhhhhhhhhdddddd
+dddddddddddddhhmNNNNmmdhhhso+/:://++sso:/sysyyyhhhhhhhdddddd
+ddddddddddddhhdNNNNmdyyso+//::::--------:/shhdddhhhhhhhddddd
+ddddddddddddhhdNNmddyss+//:://::-.....----/sddhhdhhhhhhhdddd
+ddddddddddddhhhNNmdhhyso/::-:::/::-::------:shdooshhhhhhdddd
+ddddddddddddhhhdNmddhyyys++/+/+ssso+/:::+++/:::-/ohhhhhhdddd
+ddddddddddddhhhhmNmdmdhysyyhy/:oosys+://----::+/:shhhhhhdddd
+ddddddddddddhhhhmNmmmddhyshmh:.:////::-------::/:yhhhhhhdddd
+dddddddddddddhhhhdmdhyysssyhy:...-///:-.------:-/hhhhhhddddd
+dddddddddddddhhhhhdddho++sdhy/..--/os+:-------:+hdhhhhdddddd
+ddddddddddddddhhhhhddhs++shhho:::--:/++:-----::/hNNmdhdddddd
+mddddddddddddddhhhhdmdho+oyhys+++//::/+/----::://yMMmddddddd
+mddddddddddddddddhhhmmds++sddhs+/:----:::-::::://+dNmdmddddd
+dmddddddddddddddddhhhmmdy+oyyyoo+/::----::::::://ommmmmmmmmm
+mmmdddddddddddddddddhdmmmddyssso/::---:::::::::/+dNmmmmmmmNN
+mmmmmddddddddddddddddddNmmmmdhyo//:::::::::::::+dNmmmmmmmmNN
+mmmmmdddddddddddddddmNMMmmmmmmdhyso+//:///:://odNmmmmmmmmNNN
+mmmmmmmmdddddddddmNMNNMMNmmmmmmmdyso+++////+ohmmmmmmmmmmNNNN
+mmmmmmmmmmdddmmNMMMMNNNNNNmmmmmmmdhysoooosydmNmmmmmmmmmNNNNN
+mmmmmmmmmmmNNNMMMMMMNNNNNmmmmmdddmmdhhddmmNNNNNmmmmmmNNNNNNN
+mmmmmmmmNNNNNMMMMMMNMMNNNmmddddhhyhhddmNNNNNNMMMNNNNNNNNNNNN
+mmmmmNNNNNNNNNNMMMMNMMMMNNMMNdyyyyyhdmNNNNNNNNNNNNNNNNNNNNNN
+mmNNNNNNNNNNNNNNNMMMMMMMMNMMMMNdyyhdmNNNNNNNNNNNNNNNNNNNNNNN]])
