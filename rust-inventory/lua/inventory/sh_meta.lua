df7570c57f35541cb2a2c0abca2113573dc607f6
Sir Francis Billard <sirfrancisbillard@gmail.com>
2017-03-07 10:19:36 -1000

    Update sh_meta.lua

--- rust-inventory/lua/inventory/sh_meta.lua
+++ rust-inventory/lua/inventory/sh_meta.lua
@@ -4,6 +4,43 @@ function PLAYER:IsInventoryFull()
 	return #self.Inventory >= 30
 end
 
+function PLAYER:HasItem(id, amt)
+	amt = amt or 1
+	local found = 0
+	for i = 1, 30 do
+		if self.Inventory[i].ID == id then
+			found = found + self.Inventory[i].Quantity
+			if found >= amt then
+				return true
+			end
+		end
+	end
+	return false
+end
+
+function PLAYER:IsItemOnHotbar(id, amt)
+	amt = amt or 1
+	local found = 0
+	for i = 25, 30 do
+		if self.Inventory[i].ID == id then
+			found = found + self.Inventory[i].Quantity
+			if found >= amt then
+				return true
+			end
+		end
+	end
+	return false
+end
+
+function PLAYER:HasAvailableInventorySlot()
+	for i = 1, 30 do
+		if not self.Inventory[i].ID == "empty" then
+			return true
+		end
+	end
+	return false
+end
+
 function PLAYER:NextAvailableInventorySlot()
 	for i = 1, 30 do
 		if self.Inventory[i].ID == "empty" then
