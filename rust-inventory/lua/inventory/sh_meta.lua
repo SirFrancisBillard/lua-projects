de089ab1aced9d26b8047debec8db813b2992ee3
Sir Francis Billard <sirfrancisbillard@gmail.com>
2017-03-07 07:54:43 -1000

    Update sh_meta.lua

--- rust-inventory/lua/inventory/sh_meta.lua
+++ rust-inventory/lua/inventory/sh_meta.lua
@@ -1,1 +1,14 @@
 local PLAYER = FindMetaTable("Player")
+
+function PLAYER:IsInventoryFull()
+	return #self.Inventory >= 30
+end
+
+function PLAYER:NextAvailableInventorySlot()
+	for i = 1, 30 do
+		if self.Inventory[i].ID == "empty" then
+			return i
+		end
+	end
+	return 1
+end
