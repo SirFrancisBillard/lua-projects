a243bbbe5f06a1e8983443aee14276ece76a0d04
Sir Francis Billard <sirfrancisbillard@gmail.com>
2017-04-12 15:52:26 -1000

    nice

---
+++ awesome_sweps/lua/weapons/weapon_grenadelauncher.lua
@@ -0,0 +1,30 @@
+AddCSLuaFile()
+
+SWEP.PrintName = "Grenade Launcher"
+
+SWEP.ViewModel			= "models/weapons/c_shotgun.mdl"
+SWEP.WorldModel			= "models/weapons/w_shotgun.mdl"
+
+SWEP.Spawnable = true
+SWEP.AdminOnly = false
+
+SWEP.Primary.ClipSize = -1
+SWEP.Primary.DefaultClip = 4
+SWEP.Primary.Automatic = false
+SWEP.Primary.Ammo = "none"
+
+SWEP.Secondary.ClipSize = -1
+SWEP.Secondary.DefaultClip = -1
+SWEP.Secondary.Automatic = false
+SWEP.Secondary.Ammo = "none"
+
+SWEP.Weight = 5
+SWEP.AutoSwitchTo = false
+SWEP.AutoSwitchFrom = false
+
+SWEP.Slot = 0
+SWEP.SlotPos = 1
+SWEP.DrawAmmo = true
+SWEP.DrawCrosshair = true
+
+-- nice
